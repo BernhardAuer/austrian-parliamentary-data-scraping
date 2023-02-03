8efac11a920ba3b8b96e1d8766fa8e666ccf95bb
Bernhard Auer <auer.bernie@gmail.com>
2023-02-03 23:56:06 +0100

    no db writes

--- dataScraper/dataScraper/startScraping/__init__.py
+++ dataScraper/dataScraper/startScraping/__init__.py
@@ -1,24 +1,37 @@
 import logging
-
 import azure.functions as func
+
+from spiders.NationalCouncilMeeting_spider import NationalCouncilMeetingSpider
+from pipelines import MongoDBPipeline
+
+from pymongo import MongoClient
+
+import scrapy
+import azure.functions
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+#from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+
+# https://victorsanner.nl/azure/scraping/container/instances/docker/2022/04/25/cheap-and-easy-scraping-using-scrapy-docker-and-azure-container-instances.html
+# https://doc.scrapy.org/en/latest/topics/practices.html#run-from-script
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # process = CrawlerProcess(settings)
+    # process.crawl(NationalCouncilMeetingSpider)
+    # process.start() # the script will block here until the crawling is finished
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
+    runner = CrawlerRunner(get_project_settings())
+
+    d = runner.crawl(NationalCouncilMeetingSpider)
+    d.addBoth(lambda _: reactor.stop())
+    reactor.run() # the script will block here until the crawling is finished
+    
+    return func.HttpResponse(f"Hello. This HTTP function executed successfully.")
+
